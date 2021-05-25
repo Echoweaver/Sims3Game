b32c161d6dcb5f2e7e5a3dbeb6cccda06c729fc1
Susan Rati Lane <slane@illation.net>
2021-05-25 15:32:12 -0400

    Clean up PetBreedFix loader

--- PetBreeding/Loader.cs
+++ PetBreeding/Loader.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Sims3.Gameplay.Actors;
 using Sims3.Gameplay.EventSystem;
+using Sims3.Gameplay.Skills;
 using Sims3.SimIFace;
 using Sims3.UI;
 using Sims3.UI.Hud;
@@ -46,50 +47,7 @@ namespace Echoweaver.Sims3Game.PetBreeding
             {
                 AddBreedInteractions(pet);
             }
-            EventTracker.AddListener(EventTypeId.kNewPet, new ProcessEventDelegate(OnNewPet));
-            EventTracker.AddListener(EventTypeId.kPetHadOffspring, new ProcessEventDelegate(OnOffspring));
             EventTracker.AddListener(EventTypeId.kNewOffspringPet, new ProcessEventDelegate(OnNewOffspringPet));
-
-            EventTracker.AddListener(EventTypeId.kChangeOutfit, new ProcessEventDelegate(OnChangeOutfit));
-            EventTracker.AddListener(EventTypeId.kChangeFacialHair, new ProcessEventDelegate(OnChangeFacialHair));
-            EventTracker.AddListener(EventTypeId.kChangeHairstyle, new ProcessEventDelegate(OnChangeHairstyle));
-        }
-
-        public static ListenerAction OnChangeOutfit(Event e)
-        {
-            StyledNotification.Show(new StyledNotification.Format("kChangeOutfit",
-                StyledNotification.NotificationStyle.kDebugAlert));
-            return ListenerAction.Keep;
-        }
-
-        public static ListenerAction OnChangeFacialHair(Event e)
-        {
-            StyledNotification.Show(new StyledNotification.Format("kChangeFacialHair",
-                StyledNotification.NotificationStyle.kDebugAlert));
-            return ListenerAction.Keep;
-        }
-
-        public static ListenerAction OnChangeHairstyle(Event e)
-        {
-            StyledNotification.Show(new StyledNotification.Format("kChangeHairstyle",
-                StyledNotification.NotificationStyle.kDebugAlert));
-            return ListenerAction.Keep;
-        }
-
-        public static ListenerAction OnNewPet(Event e)
-        {
-            StyledNotification.Show(new StyledNotification.Format("New Pet Event",
-                StyledNotification.NotificationStyle.kDebugAlert));
-            AddBreedInteractions(e.TargetObject as Sim);
-            return ListenerAction.Keep;
-        }
-
-        public static ListenerAction OnOffspring(Event e)
-        {
-            StyledNotification.Show(new StyledNotification.Format("Pet Had Offspring Event",
-                StyledNotification.NotificationStyle.kDebugAlert));
-            AddBreedInteractions(e.TargetObject as Sim);
-            return ListenerAction.Keep;
         }
 
         public static ListenerAction OnNewOffspringPet(Event e)
