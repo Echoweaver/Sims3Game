d1bb7de2233f08862d3c6015dbb3c919a1e1c2a9
Susan Rati Lane <slane@illation.net>
2022-02-11 20:40:32 -0500

    Move Wait interaction to its own file

---
+++ WarriorCats/EWWait.cs
@@ -0,0 +1,46 @@
+﻿using Sims3.Gameplay.Actors;
+using Sims3.Gameplay.Interactions;
+using Sims3.SimIFace;
+
+namespace Echoweaver.Sims3Game.WarriorCats
+{
+	public class EWWait : Interaction<Sim, Sim>, IInteractionNameCanBeOverriden
+	{
+		public class Definition : InteractionDefinition<Sim, Sim, EWWait>
+		{
+			public static InteractionDefinition Singleton = new Definition();
+
+			public override bool Test(Sim actor, Sim target, bool isAutonomous,
+				ref GreyedOutTooltipCallback greyedOutTooltipCallback)
+			{
+				return true;
+			}
+		}
+
+		public static InteractionDefinition Singleton = new Definition();
+
+		public string mOverrideInteractionName = "Wait";
+
+		public bool waitComplete = false;
+
+		public override bool Run()
+		{
+			while (!Actor.WaitForExitReason(Sim.kWaitForExitReasonDefaultTime, ExitReason.Canceled)
+				&& !waitComplete)
+			{
+				Actor.LoopIdle();
+			}
+			return true;
+		}
+
+		public override string GetInteractionName()
+		{
+			return mOverrideInteractionName;
+		}
+
+		public void SetInteractionName(string interactionName)
+		{
+			mOverrideInteractionName = interactionName;
+		}
+	}
+}
