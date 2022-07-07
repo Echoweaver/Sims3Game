0f27ff767d3b6de60f432c9557daa4a08456e738
Susan Rati Lane <slane@illation.net>
2022-07-07 18:11:50 -0400

    Wheat: Gardening and flour grinding functionality works. Wheat models need work. Recipes need adding.

---
+++ PlantableWheat/EWGrindFlour.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections.Generic;
+using Sims3.Gameplay;
+using Sims3.Gameplay.Actors;
+using Sims3.Gameplay.ActorSystems;
+using Sims3.Gameplay.Interactions;
+using Sims3.Gameplay.Interfaces;
+using Sims3.Gameplay.Objects;
+using Sims3.Gameplay.Objects.Appliances;
+using Sims3.Gameplay.Objects.CookingObjects;
+using Sims3.Gameplay.Objects.Counters;
+using Sims3.Gameplay.Objects.FoodObjects;
+using Sims3.Gameplay.Objects.Seating;
+using Sims3.SimIFace;
+using Sims3.UI;
+using static Sims3.Gameplay.Objects.Appliances.FoodProcessor;
+
+namespace Echoweaver.Sims3Game.PlantableWheat
+{
+	public class EWGrindFlour : Interaction<Sim, FoodProcessor>
+	{
+		public class Definition : InteractionDefinition<Sim, FoodProcessor, EWGrindFlour>
+		{
+			public override bool Test(Sim a, FoodProcessor target, bool isAutonomous,
+				ref GreyedOutTooltipCallback greyedOutTooltipCallback)
+			{
+                Ingredient i = a.Inventory.Find<Ingredient>(WheatTest);
+                if (i != null)
+                {
+                    return !target.InUse;
+                } else
+                {
+					greyedOutTooltipCallback = CreateTooltipCallback("You must have wheat in inventory.");
+				}
+				return false;
+			}
+
+			public static bool WheatTest(IGameObject obj, object customData)
+			{
+				Ingredient ingredient = obj as Ingredient;
+				if (ingredient != null && ingredient.IngredientKey == "EWWheat")
+				{
+					return true;
+				}
+				return true;
+			}
+
+            public override string GetInteractionName(ref InteractionInstanceParameters parameters)
+            {
+                return "Grind Flour";
+            }
+		}
+
+		public HighChair HighChair;
+
+		public static InteractionDefinition Singleton = new Definition();
+
+		public void SetFoodProcessorGeoState(StateMachineClient smc, IEvent evt)
+		{
+			Target.SetGeometryState("full");
+			Target.SetMaterial("FoodSludge");
+		}
+
+		public override bool Run()
+		{
+			if (CheckForCancelAndCleanup())
+			{
+				return false;
+			}
+			Ingredient wheatItem = Actor.Inventory.Find<Ingredient>(Definition.WheatTest);
+			if (wheatItem == null)
+			{
+				return false;
+			}
+			CarrySystem.PickUpFromSimInventory(Actor, wheatItem);
+            if (!Target.RouteAndCheckInUse(Actor))
+			{
+				return false;
+			}
+			StandardEntry();
+			EnterStateMachine("FoodProcessor", "Enter - Plate", "x", "FoodProcessor");
+			SetParameter("wasInterrupted", false);
+			SetActor("Start Platter", wheatItem);
+			BowlLarge foodBowl = GlobalFunctions.CreateObject("BowlLarge", Vector3.OutOfWorld, 0,
+				Vector3.UnitZ) as BowlLarge;
+			SetActor("Stop Platter", foodBowl);
+
+			FoodProp foodProp = FoodProp.Create("foodPrepAngelFoodCake");
+            FoodTransferHelper foodTransferHelper = new FoodTransferHelper(wheatItem);
+			CarrySystem.PickUpFromSimInventory(Actor, foodBowl);
+			PutInContainerHelper putInContainerHelper = new PutInContainerHelper(Target,
+				foodBowl, foodProp, ServingContainer.kContainmentSlot);
+			PlayFoodVFXHelper playFoodVFXHelper = new PlayFoodVFXHelper(Target, Slot.FXJoint_0,
+				"foodProcessorPrepLeftHand");
+			AddOneShotScriptEventHandler(100u, new SacsEventHandler(playFoodVFXHelper.Callback));
+			AddOneShotScriptEventHandler(108u, new SacsEventHandler(SetFoodProcessorGeoState));
+			AddOneShotScriptEventHandler(102u, new SacsEventHandler(foodTransferHelper.HideFood));
+			AddOneShotScriptEventHandler(109u, new SacsEventHandler(foodTransferHelper.Destroy));
+			AddOneShotScriptEventHandler(103u, new SacsEventHandler(putInContainerHelper.Callback));
+			AddOneShotScriptEventHandler(1001u, new SacsEventHandler(Target.StartSound));
+			AddOneShotScriptEventHandler(1002u, new SacsEventHandler(Target.StopSound));
+			BeginCommodityUpdates();
+			AnimateSim("Process");
+			AnimateSim("Exit - Bowl");
+			//CarrySystem.EnterWhileHolding(Actor, foodBowl);
+            CarrySystem.AnimateIntoSimInventory(Actor);
+            DestroyObject(foodBowl);
+            StandardExit();
+			Counter counter = Target.Parent as Counter;
+			if (counter != null && counter.IsCleanable)
+			{
+				counter.Cleanable.DirtyInc(Actor);
+			}
+			Quality flourQuality = wheatItem.GetQuality();
+			wheatItem.Dispose();
+			return AddIngredientsToSimInventory(Actor, "Flour", 1, flourQuality); 
+		}
+
+		public static bool AddIngredientsToSimInventory(Sim sim, string key, int number, Quality quality)
+		{
+			if (sim == null)
+			{
+				return false;
+			}
+			List<IGameObject> ingredientStack = new List<IGameObject>();
+			for (int i = 0; i < number; i++)
+			{
+				IGameObject gameObject = null;
+				IngredientData value = null;
+				if (IngredientData.NameToDataMap.TryGetValue(key, out value))
+				{
+					gameObject = Ingredient.Create(value, quality, false,
+						Sims3.Gameplay.Objects.Gardening.PlayerDisclosure.Exposed);
+				}
+				else
+				{
+					return false;
+				}
+				ingredientStack.Add(gameObject);
+			}
+			if (ingredientStack.Count > 0)
+			{
+				return sim.Inventory.TryToAddStack(ingredientStack);
+			}
+			return false;
+		}
+	}
+}
---
+++ PlantableWheat/WheatLoader.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Sims3.Gameplay.Abstracts;
+using Sims3.Gameplay.EventSystem;
+using Sims3.Gameplay.Objects.Appliances;
+using Sims3.Gameplay.Objects.FoodObjects;
+using Sims3.Gameplay.Objects.Gardening;
+using Sims3.Gameplay.Objects.RabbitHoles;
+using Sims3.Gameplay.Utilities;
+using Sims3.SimIFace;
+using Sims3.UI;
+
+namespace Echoweaver.Sims3Game.PlantableWheat
+{
+    public class Loader
+    {
+        static bool HasBeenLoaded = false;
+
+        [Tunable]
+        protected static bool kInstantiator = false;
+
+        static Loader()
+        {
+            World.sOnWorldLoadStartedEventHandler += new EventHandler(OnWorldLoadStarted);
+            World.sOnWorldLoadFinishedEventHandler += new EventHandler(OnWorldLoadFinished);
+        }
+
+        public static void OnWorldLoadFinished(object sender, System.EventArgs e)
+        {
+            foreach (GameObject o in Sims3.Gameplay.Queries.GetObjects<FoodProcessor>())
+            {
+                o.AddInteraction(EWGrindFlour.Singleton);
+            }
+            // TODO: Is this the best way to check for a newly added object?
+            EventTracker.AddListener(EventTypeId.kBoughtObject, new ProcessEventDelegate(OnNewObject));
+        }
+
+        public static ListenerAction OnNewObject(Event e)
+        {
+            // TODO: Add interaction to new food processors
+            return ListenerAction.Keep;
+        }
+
+        public static void OnWorldLoadStarted(object sender, System.EventArgs e)
+        {
+            if (HasBeenLoaded) return; // you only want to run it once per gameplay session
+            HasBeenLoaded = true;
+
+            string error_list = "";
+            XmlDbData data = XmlDbData.ReadData(new ResourceKey(ResourceUtils.HashString64("WheatPlant"),
+                0x0333406C, 0x00000000), false);
+
+            if (data != null)
+            {
+                PlantDefinition.ParsePlantDefinitionData(data, false);
+            } else
+            {
+                error_list += "Plant data null.";
+            }
+
+            data = null;
+            data = XmlDbData.ReadData(new ResourceKey(ResourceUtils.HashString64("WheatIngredients"),
+                0x0333406C, 0x00000000), false);
+
+            if (data != null)
+            {
+                IngredientData.LoadIngredientData(data, false);
+                Grocery.mItemDictionary.Clear();
+                Grocery.LoadData();
+            }
+            else
+            {
+                error_list += "  Ingredient data null.";
+            }
+
+            data = null;
+            data = XmlDbData.ReadData(new ResourceKey(ResourceUtils.HashString64("WheatRecipes"),
+                0x0333406C, 0x00000000), false);
+
+            if (data != null)
+            {
+                if (Recipe.NameToRecipeHash.ContainsKey("Pancakes"))
+                {
+                    Recipe.NameToRecipeHash.Remove("Pancakes");
+                }
+                Recipe.LoadRecipeData(data, false);
+            }
+            else
+            {
+                error_list += "  Recipe data null.";
+            }
+
+            if (!error_list.Equals(""))
+            {
+                StyledNotification.Show(new StyledNotification.Format("ERROR Plantable Wheat: " + error_list,
+                    StyledNotification.NotificationStyle.kDebugAlert));
+                return;
+            }
+        }
+    }
+}
