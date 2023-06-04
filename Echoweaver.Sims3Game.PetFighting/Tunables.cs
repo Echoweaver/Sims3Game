41507771fdec1612c6b877eab86f65a9e80f90bc
Susan Rati Lane <slane@illation.net>
2023-06-03 23:31:45 -0400

    Pet Fighting: Add tuning resource. This should be included with the last Fighting commit.

---
+++ Echoweaver.Sims3Game.PetFighting/Tunables.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Sims3.SimIFace;
+
+namespace Echoweaver.Sims3Game.PetFighting
+{
+	public class Tunables
+	{
+
+        [Tunable]
+        public static bool kAllowPetDeath = true;
+
+        [Tunable]
+        public static int kGraveWoundDuration = 720;
+
+        [Tunable]
+        public static int kSeriousWoundDuration = 480;
+
+        [Tunable]
+        public static int kMinorWoundDuration = 240;
+
+        [Tunable]
+        public static int kRecuperateDuration = 720;
+
+        [Tunable]
+        public static int kCostOfVetVisit = 200;
+
+        [Tunable]
+        public static int kLTRBoostOfVetVisit = 20;
+
+        public Tunables()
+		{
+		}
+	}
+}
+
